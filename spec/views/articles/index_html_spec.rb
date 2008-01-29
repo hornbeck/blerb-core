a1945353947c50fccbbbd4ea6447381ee880291a
Jeff Whitmire <jeff@jwhitmire.com>
2008-01-28 23:32:58 -0500

    Revised articles index view to include message when no articles exist

--- spec/views/articles/index_html_spec.rb
+++ spec/views/articles/index_html_spec.rb
@@ -1,26 +1,38 @@
 require File.join(File.dirname(__FILE__),'..','..','spec_helper')
 
-describe "/articles" do
-  before(:each) do
-    @article1 = mock("article")
-    @article2 = mock("article")
-    @article1.should_receive(:title).and_return("Article 1")
-    @article2.should_receive(:title).and_return("Article 2")    
-    Article.stub!(:all).and_return([@article1,@article2])
+describe "index.html.erb" do
+  describe "when articles exist" do
+    before(:each) do
+      @article1 = mock("article")
+      @article2 = mock("article")
+      @article1.should_receive(:title).and_return("Article 1")
+      @article2.should_receive(:title).and_return("Article 2")    
+      Article.stub!(:all).and_return([@article1,@article2])
 
-    @body = fake_render 'articles/index.html.erb', :articles => Article.all
+      @body = fake_render 'articles/index.html.erb', :articles => Article.all
+    end
+
+    it "should mention Articles" do
+      @body.should match(/Articles/)
+    end
+  
+    it "should have page title in h1" do
+      @body.should include("<h1>Latest Articles</h1>")  
+    end
+  
+    it "should list all articles" do
+      @body.should include("<li>Article 1</li>")
+      @body.should include("<li>Article 2</li>")
+    end
   end
 
-  it "should mention Articles" do
-    @body.should match(/Articles/)
-  end
-  
-  it "should have page title in h1" do
-    @body.should include("<h1>Listing Articles</h1>")  
-  end
-  
-  it "should list all articles" do
-    @body.should include("<li>Article 1</li>")
-    @body.should include("<li>Article 2</li>")
+  describe "when noarticles have been created" do
+    before(:each) do
+      @body = fake_render 'articles/index.html.erb', :articles => []
+    end
+
+    it "should mention 'No articles created yet'" do
+      @body.should include("<h2>No articles created yet</h2>")  
+    end
   end
 end
