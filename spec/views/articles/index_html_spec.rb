feed29f396f78bd86640b9a390374d883f94921a
Jeff Whitmire <jeff@jwhitmire.com>
2008-01-28 23:58:28 -0500

    added show page for articles

--- spec/views/articles/index_html_spec.rb
+++ spec/views/articles/index_html_spec.rb
@@ -2,37 +2,44 @@ require File.join(File.dirname(__FILE__),'..','..','spec_helper')
 
 describe "index.html.erb" do
   describe "when articles exist" do
+    attr_reader :titles, :body, :header
     before(:each) do
-      @article1 = mock("article")
-      @article2 = mock("article")
-      @article1.should_receive(:title).and_return("Article 1")
-      @article2.should_receive(:title).and_return("Article 2")    
-      Article.stub!(:all).and_return([@article1,@article2])
+      @header = "Latest Articles"
+      @titles = ["Good Article", "Even Better Article"]
+      
+      article1 = mock("article")
+      article2 = mock("article")
+      article1.should_receive(:title).and_return(titles[0])
+      article2.should_receive(:title).and_return(titles[1])    
+      Article.stub!(:all).and_return([article1,article2])
 
       @body = fake_render 'articles/index.html.erb', :articles => Article.all
     end
 
     it "should mention Articles" do
-      @body.should match(/Articles/)
+      body.should match(/Articles/)
     end
   
     it "should have page title in h1" do
-      @body.should include("<h1>Latest Articles</h1>")  
+      body.should include("<h1>#{header}</h1>")  
     end
   
     it "should list all articles" do
-      @body.should include("<li>Article 1</li>")
-      @body.should include("<li>Article 2</li>")
+      titles.each { |title|
+        body.should include("<li>#{title}</li>")
+      }
     end
   end
 
   describe "when noarticles have been created" do
+    attr_reader :body, :no_articles_text
     before(:each) do
+      @no_articles_text = "No articles created yet"
       @body = fake_render 'articles/index.html.erb', :articles => []
     end
 
-    it "should mention 'No articles created yet'" do
-      @body.should include("<h2>No articles created yet</h2>")  
+    it "should mention no articles have been created" do
+      body.should include("<h2>#{no_articles_text}</h2>")  
     end
   end
 end
