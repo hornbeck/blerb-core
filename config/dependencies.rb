72505c90522eea392053c33a30acf79f6e7a801d
Ben Burkert <ben@ben-burkerts-macbook.local>
2008-01-31 16:54:26 -0600

    changed named route to url() route

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -33,6 +33,7 @@ use_test :rspec
 # dependency "RedCloth", "> 3.0"
 # OR
 # dependencies "RedCloth" => "> 3.0", "ruby-aes-cext" => "= 1.0"
+dependencies "merb_stories"
 
 Merb::BootLoader.after_app_loads do
   ### Add dependencies here that must load after the application loads:
