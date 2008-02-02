c554e37a6855975d8aba7050fb0696150512dbb0
Ben Burkert <ben@benburkert.com>
2008-02-02 17:17:02 -0600

    Finished revamp of boxy steps, now uber fast and flexible

--- stories/helpers/has_tag_helper.rb
+++ stories/helpers/has_tag_helper.rb
@@ -9,7 +9,7 @@ class HasTag
     (@tag, @filter), @attributes = args_and_options(*args)
   end
   
-  def matches?(stringlike)
+  def matches?(stringlike, &block)
     @document = case stringlike
     when Hpricot::Elem
       stringlike
@@ -19,18 +19,25 @@ class HasTag
       Hpricot.parse(stringlike)
     end
     
-    not @document.search(selector()).empty?
+    not @document.search(selector(&block)).empty?
   end
   
-  def selector
+  def selector(&block)
+    
     start = "//#{@tag}#{'#' if @attributes.has_key?(:id)}#{@attributes.delete(:id)}#{'.' if @attributes.has_key?(:class)}#{@attributes.delete(:class)}"
-    @attributes.to_a.inject(start) do |select, (key, value)|
+    @selector = @attributes.to_a.inject(start) do |select, (key, value)|
       select << "[@#{key}=\"#{value}\"]"
       select
     end
+    
+    @selector << @inner_has_tag.selector if (@inner_has_tag = block.call).is_a?(HasTag) unless block.nil?
+    
+    @selector
   end
 end
 
 def have_tag(*args)
   HasTag.new(*args)
 end
+
+alias :with_tag :have_tag
--- stories/steps/boxy_steps.rb
+++ stories/steps/boxy_steps.rb
@@ -1,3 +1,24 @@
+# Boxy Steps
+#
+# Boxy Steps is an attempt at a convention for creating story tests
+# that check html output.  This should make it easy to test for the
+# existence of elements in the DOM tree.
+#
+# Currently their are four types of checks:
+#   for an element
+#   for an element containing a string or regexp
+#   for an element with an attribute
+#   for an element within another element
+#
+# Instead of specifying the class or id explicitly, Boxy Steps follow
+# a convention of using an article before the identifier value to signal
+# either class or id.  'a' and 'an' for class, 'the' for id.
+#
+# For example, "a round div" will match <div class='round'>
+# "the signup subtitle" will match <h2 id='signup'>
+#
+# Also, there is aliasing for elements, so that in the previous example,
+# subtitle will be translated to h2
 steps_for(:boxy) do
   
   start_of_boxy_step = "they should see "
@@ -27,6 +48,8 @@ steps_for(:boxy) do
     @response.body.should have_tag(tag, filter, attributes)
   end
   
+  #example:
+  #Then they should see a hentry box
   Then "#{start_of_boxy_step}$parent#{end_of_boxy_step}" do |parent|
     tag, filter, attributes = tag_filter_and_attributes(parent)
     
